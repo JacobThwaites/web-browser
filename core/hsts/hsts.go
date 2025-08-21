80a2cbf1b8a17560c516366c2cfb3a174673a294
Jacob Thwaites <jakethwaites@gmail.com>
2025-08-21 20:19:14 +0200

    store HSTS info in map and create function to get entry by domain name

--- core/hsts/hsts.go
+++ core/hsts/hsts.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/tidwall/gjson"
 )
+
+var hstsList map[string]EntryInfo
 
 func fetchHsts() {
     url := "https://raw.githubusercontent.com/chromium/chromium/refs/heads/master/net/http/transport_security_state_static.json"
@@ -14,7 +17,6 @@ func fetchHsts() {
     // Fetch the data
     resp, err := http.Get(url)
     if err != nil {
-        fmt.Println("asdfasdf")
         panic(err)
     }
     defer resp.Body.Close()
@@ -42,6 +44,48 @@ func fetchHsts() {
     }
 }
 
+type EntryInfo struct {
+    Mode              string
+	Policy            string
+	IncludeSubdomains bool
+    Pins              string
+}
+
+func parseEntries(data []byte) map[string]EntryInfo {
+	results := gjson.GetBytes(data, "entries")
+    entries := make(map[string]EntryInfo)
+
+	results.ForEach(func(_, value gjson.Result) bool {
+        name := value.Get("name").String()
+        info := EntryInfo{
+            Mode: value.Get("mode").String(),
+            Policy: value.Get("policy").String(),
+            IncludeSubdomains: value.Get("include_subdomains").Bool(),
+            Pins: value.Get("pins").String(),
+        }
+
+        entries[name] = info
+		return true // keep iterating
+	})
+
+	return entries
+}
+
+func loadHsts() {
+    data, err := os.ReadFile("entries.json")
+	if err != nil {
+		panic(err)
+	}
+
+    hstsList = parseEntries(data)
+}
+
+func GetHstsByDomain(domain string) (EntryInfo, bool) {
+    e, ok := hstsList[domain]
+	return e, ok
+}
+
 func main() {
     fetchHsts()
+    loadHsts()
 }
